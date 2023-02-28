10264b43ae006e3fdbf8d24752bc1d5ff0161d41
gabrielmmh <gabrielmmh@al.insper.edu.br>
2023-02-28 15:01:12 -0300

    Implementando o build_response()

---
+++ templates/database.py
@@ -0,0 +1,40 @@
+import sqlite3
+from dataclasses import dataclass
+
+@dataclass
+class Note:
+    id: int = None
+    title: str = None
+    content: str = ''
+
+class Database:
+    def __init__(self, db_name):
+        self.conn = sqlite3.connect(f'{db_name}.db')
+        self.conn.execute('CREATE TABLE IF NOT EXISTS note (id INTEGER PRIMARY KEY, title TEXT, content TEXT NOT NULL);')
+
+    def add(self, note):
+        self.conn.execute('INSERT INTO note (title, content) VALUES (?, ?)', (note.title, note.content))
+        self.conn.commit()
+
+    def get_all(self):
+        lista = []
+        cursor = self.conn.execute('SELECT id, title, content FROM note')
+        for linha in cursor:
+            id = linha[0]
+            title = linha[1]
+            content = linha[2]
+            lista.append(Note(id, title, content))
+        return lista
+
+    def update(self, entry):
+        self.conn.execute('UPDATE note SET title = ?, content = ? WHERE id = ?', (entry.title, entry.content, entry.id))
+        self.conn.commit()
+
+    # Implemente o método delete(self, note_id), que recebe o valor de um id e apaga essa entrada do banco de dados. Obs: lembre-se de chamar o método commit depois do execute.
+
+    # Se tudo der certo. O teste test_delete_row e todos os outros devem passar com sucesso.
+
+    def delete(self, note_id):
+        self.conn.execute(f'DELETE FROM note WHERE id = {note_id}')
+        self.conn.commit()
+        
---
+++ utils.py
@@ -0,0 +1,48 @@
+import json
+from database import *
+
+def extract_route(request):
+    """Implemente a função extract_route, que recebe uma string com a requisição e devolve a rota, excluindo o primeiro caractere (/).
+    """
+    return request.split()[1][1:]
+
+def read_file(filepath):
+    """Implemente a função read_file, que recebe um caminho de arquivo e devolve o conteúdo do arquivo.
+    """
+    with open(filepath, 'rb') as f:
+        return f.read()
+
+def load_data(banco):
+    """Implemente a função load_data, que recebe um caminho de arquivo e devolve um dicionário com os dados contidos no arquivo.
+    """
+    db = Database(f'{banco}')
+
+    notes = db.get_all()
+
+    return notes
+
+def load_template(filepath):
+    """Implemente a função load_template, que recebe um caminho de arquivo e devolve uma string com o conteúdo do arquivo.
+    """
+    with open("templates/" + filepath, 'r', encoding='utf-8') as f:
+        return f.read()
+    
+def formata_nota(nota):
+    # title = chave da nota
+    # details = valor da nota
+    return {'titulo': nota.keys(), 'detalhes': nota.values()}
+
+def adicionar_nota(nota):
+    """Implemente a função adicionar_nota, que recebe um dicionário com os dados da nota e adiciona a nota ao arquivo notes.json.
+    """
+    db = Database('banco')
+    size = len(nota)
+    note = Note(title=nota['titulo'], content=nota['detalhes'])
+    db.add(note)
+
+def build_response(body='', code=200, reason='OK', headers=''):
+    """Implemente a função build_response, que recebe um corpo, um código, um motivo e um cabeçalho e devolve uma string com a resposta HTTP.
+    """
+    return f'HTTP/1.1 {code} {reason} \n {headers} \n {body}'  
+
+    
